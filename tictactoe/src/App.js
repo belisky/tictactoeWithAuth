3d9b65a511bc6f141d2e8dc8829e1916dc4b4c17
bel <denoblesnobility2@gmail.com>
2021-12-01 10:29:23 +0000

    added router from react-router-dom

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,11 +1,15 @@
-//import GamePage from './Pages/GamePage/GamePage'
-//import GameHome from './Pages/GameHome/GameHome';
+import GamePage from './Pages/GamePage/GamePage'
+import GameHome from './Pages/GameHome/GameHome';
 import LoginRegister from "./Pages/LoginRegister/LoginRegister";
+import {Routes,Route} from 'react-router-dom'
 function App() {
   return (
     <div className="board">
-     
-      <LoginRegister />
+     <Routes>
+       <Route path="/" element={<LoginRegister />}/>
+       <Route path="/gamehome/" element={<GameHome/>}/>
+       <Route path="/gamepage/" element={<GamePage/>}/>      
+      </Routes>
     </div>
   );
 }
