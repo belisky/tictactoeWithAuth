5a006fedbffff09dae0cf0d13fe26bf08584cb0e
bel <denoblesnobility2@gmail.com>
2021-12-01 10:28:32 +0000

    done with register/login page

--- tictactoe/src/Pages/LoginRegister/LoginRegister.jsx
+++ tictactoe/src/Pages/LoginRegister/LoginRegister.jsx
@@ -1,32 +1,44 @@
 import './LoginRegister.css'
-import React from 'react'
+import React,{useState} from 'react'
 
 const LoginRegister = () => {
+    const [toggle,setToggle]=useState(true)
+
+    const toggleForms=()=> {
+        setToggle(false);
+    }
+    const toggleRForms=()=> {
+        setToggle(true);
+    }
+     
+
     return (
         <div className="register-page">
             <div className="card">
                 <div className="headings">
  
-                    <label className="heading__item" htmlFor="toggle-login">Login</label>
-                    <p className="heading__item">Register</p>
+                    <p onClick={toggleForms} className={`heading__item  ${toggle ? "":"heading__login"}`} >Login</p>
+                    <p onClick={toggleRForms} className={`heading__item  ${!toggle ? "":"heading__login"}`}>Register </p>
                 </div>
-                <input type="checkbox" className="toggle-login" id="toggle-login"/>
-                <div className="forms">
+                <div className={`forms${toggle ? "-t":""}`}>
                     
                     <p className="form__heading">Login</p>
-                    <label className="labels">Name</label>
-                    <input type="text" className="inputs"/>
-                    <label className="labels">Password</label>
-                    <input type="password" className="inputs" />
+                    
+                    <input type="text" className="inputs" placeholder="Name"/>
+                     
+                    <input type="password" className="inputs" placeholder="Password"/>
                     <button className="submit-btn">Login</button>
+                    <p className="no-padding">Forgot password? reset here!</p>
+                    <p className="no-padding">New here, Register instead!!</p>
                 </div>
-                <div className="register-forms">                    
+                <div className={`register-forms${toggle ? "-t":""}`}>                    
                     <p className="form__heading">Register</p>
-                    <label className="labels">Name</label>
-                    <input type="text" className="inputs"/>
-                    <label className="labels">Password</label>
-                    <input type="password" className="inputs" />
+                    <input type="text" className="inputs" placeholder="Name"/>
+                     
+                    <input type="password" className="inputs" placeholder="Password"/>
                     <button className="submit-btn">Sign Up</button>
+                    <p className="no-padding">Already have an account? Login</p>
+                   
                 </div>
             
             </div>
