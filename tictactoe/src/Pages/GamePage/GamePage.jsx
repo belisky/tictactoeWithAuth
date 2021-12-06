450070f92a7327c7a1f8785d136bbc8a66c3c076
bel <denoblesnobility2@gmail.com>
2021-12-06 18:44:37 +0000

    added a logout functionality

--- tictactoe/src/Pages/GamePage/GamePage.jsx
+++ tictactoe/src/Pages/GamePage/GamePage.jsx
@@ -2,6 +2,8 @@ import React  from 'react'
 import GamePageLogic from './GamePageLogic'
 import './GamePage.css'
 import { useNavigate } from 'react-router'
+import {signOut } from '@firebase/auth'
+import { auth } from '../../Config/firebase-config'
  
 
 const GamePage = () => {
@@ -15,6 +17,15 @@ const GamePage = () => {
         setBoard(["","","","","","","","",""]);
         setPlayer("O");
     }
+    const logout=async()=>{
+        try{
+            await signOut(auth);
+            navigate("/")
+        } catch(error) {
+            console.log(error.message);
+        }
+
+    }
     
     return (
         <div  >
@@ -23,6 +34,7 @@ const GamePage = () => {
                 <ul className="options__menu">
                     <li onClick={quit} className="options__list">Quit</li>
                     <li onClick={reset} className="options__list" >Restart</li>
+                    <li onClick={logout} className="options__list" >Logout</li>
                 </ul>
                
                 <div className="timer">
